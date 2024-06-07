1f7e0956715fe2888c2374c016c60f5aba7fede6
mauriciozorzan <dr.maukz@gmail.com>
2024-06-07 22:27:49 +0700

    deploy to gh-pages

---
+++ client/build/assets/cleanCurrencyAddress-507bc2cf.browser.esm-e64bba82.js
@@ -0,0 +1,1 @@
+import{bD as e,bi as i}from"./index-f129da69.js";function t(r){return e(r)?i:r}export{t as c};
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode (development or production)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    base: '/CrowdFundingApp/', // Ensure this matches your GitHub repository name
+    build: {
+      outDir: 'build', // Specify the output directory for the build
+    },
+    define: {
+      'process.env': env, // Load environment variables
+    },
+  };
 });
